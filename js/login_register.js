afb7e71ad16fb80d449260b4a73483ae54909781
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2023-02-18 14:40:36 +0100

    Nu har jag fixat fetch funktionen samt login och registrerings funktionen

--- js/login_register.js
+++ js/login_register.js
@@ -9,15 +9,58 @@ function make_register_page(event) {
         document.querySelector("h1").textContent = "REGISTER"
         document.getElementById("register").textContent = "Already have an account? Go to login"
         document.querySelector("h3").textContent = "Ready when you are..."
-        document.querySelector("button").textContent = "Register"
+        document.querySelector("#login_or_register").textContent = "Register"
     } else {
         document.querySelector("h1").textContent = "LOGIN"
         document.getElementById("register").textContent = "New to this? Register for free"
         document.querySelector("h3").textContent = "let the magic start"
-        document.querySelector("button").textContent = "Login"
+        document.querySelector("#login_or_register").textContent = "Login"
     }
 
 
+
+}
+
+document.querySelector("#login_or_register").addEventListener("click", username_or_login)
+
+async function username_or_login(event) {
+
+    let user_name = document.querySelector("#user_name").value
+    let password = document.querySelector("#password").value
+
+
+    let body_post = {
+        action: "register",
+        user_name: `${user_name}`,
+        password: `${password}`,
+    }
+
+    const options = {
+        method: `POST`,
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify(body_post),
+    }
+
+
+
+    if (event.target.textContent === "Register") {
+
+        let post_rqst = new Request(`https://teaching.maumt.se/apis/access/`, options)
+
+        fetch_function(post_rqst)
+    }
+    if (event.target.textContent === "Login") {
+        let rqst = new Request(`https://teaching.maumt.se/apis/access/?action=check_credentials&user_name=${body_post.user_name}&password=${body_post.password}`)
+        fetch_function(rqst)
+    }
+}
+
+async function fetch_function(url) {
+    let response = await fetch(url)
+    let resource = await response.json()
+    console.log(resource);
+    console.log(response);
+    return resource
 }
 
 
