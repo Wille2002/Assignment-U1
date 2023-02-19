73e7c191838894fbbce5b53cc51f7b29d0941a4f
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2023-02-19 17:27:00 +0100

    Jag har adderat feedback rutor när man registrerar/loggar in

--- js/login_register.js
+++ js/login_register.js
@@ -6,6 +6,7 @@ function make_register_page(event) {
     let wrapper = document.getElementById("wrapper")
     wrapper.classList.toggle("register_account")
     if (wrapper.classList.contains("register_account")) {
+        document.querySelector("h3").classList.remove("wrong_login")
         document.querySelector("h1").textContent = "REGISTER"
         document.getElementById("register").textContent = "Already have an account? Go to login"
         document.querySelector("h3").textContent = "Ready when you are..."
@@ -45,22 +46,120 @@ async function username_or_login(event) {
 
     if (event.target.textContent === "Register") {
 
+
         let post_rqst = new Request(`https://teaching.maumt.se/apis/access/`, options)
 
-        fetch_function(post_rqst)
+        make_feedback_square(post_rqst)
+
     }
     if (event.target.textContent === "Login") {
-        let rqst = new Request(`https://teaching.maumt.se/apis/access/?action=check_credentials&user_name=${body_post.user_name}&password=${body_post.password}`)
-        fetch_function(rqst)
+        let get_rqst = new Request(`https://teaching.maumt.se/apis/access/?action=check_credentials&user_name=${body_post.user_name}&password=${body_post.password}`)
+        feedback_login(get_rqst)
     }
 }
 
 async function fetch_function(url) {
     let response = await fetch(url)
     let resource = await response.json()
-    console.log(resource);
+    if (response.status !== 200) {
+        return response
+    }
     console.log(response);
+    console.log(resource);
     return resource
 }
 
+async function feedback_login(get_rqst) {
+    let wrapper = document.querySelector("#wrapper")
+    let body = document.querySelector("body")
+    wrapper.classList.add("feedback")
+    let feedback = document.createElement("div")
+    feedback.textContent = "Contacting server..."
+    body.appendChild(feedback)
+    feedback.classList.add("connecting")
 
+
+
+
+    let response = await fetch_function(get_rqst)
+
+    let wrong_login = document.querySelector("h3")
+
+    switch (response.status) {
+        case 200:
+            document.querySelector("h3").classList.remove("wrong_login")
+            break
+        case 418:
+            feedback.textContent = "The server think it`s not a teapot"
+
+            feedback.classList.add("feedback_square")
+            let closing_button = document.createElement("button")
+            closing_button.textContent = "CLOSE"
+            feedback.appendChild(closing_button)
+            closing_button.classList.add("feedback_button_close")
+            closing_button.addEventListener("click", remove_closing_button)
+            break;
+
+        default:
+            wrong_login.classList.add("wrong_login")
+            wrong_login.textContent = "Wrong user name or password"
+            wrapper.classList.remove("feedback")
+            feedback.remove()
+
+            break;
+
+    }
+}
+
+
+
+async function make_feedback_square(post_rqst) {
+    let wrapper = document.querySelector("#wrapper")
+    let body = document.querySelector("body")
+    wrapper.classList.add("feedback")
+    let feedback = document.createElement("div")
+    feedback.textContent = "Contacting server..."
+    body.appendChild(feedback)
+    feedback.classList.add("connecting")
+
+
+
+
+    let response = await fetch_function(post_rqst)
+
+
+    switch (response.status) {
+        case 418:
+            feedback.textContent = "The server think it`s not a teapot"
+            break;
+        case 409:
+            feedback.textContent = "Sorry that name is taken. Please try with another one"
+            break
+        case 400:
+            feedback.textContent = "Sorry that name is taken. Please try with another one"
+            break
+        default:
+            feedback.textContent = "Registration Complete. Please proceed to Login"
+
+            break;
+
+    }
+
+    feedback.classList.add("feedback_square")
+    let closing_button = document.createElement("button")
+    closing_button.textContent = "CLOSE"
+    feedback.appendChild(closing_button)
+    closing_button.classList.add("feedback_button_close")
+    closing_button.addEventListener("click", remove_closing_button)
+
+
+
+
+}
+
+function remove_closing_button(event) {
+
+    let div = document.querySelector(".feedback_square")
+    div.remove()
+    document.querySelector("#wrapper").classList.remove("feedback")
+}
